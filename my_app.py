70ccd69b3a420ed5383f1f790af3fca519c21e2d
zakariae drabech <52219383+Zakariae-drabech@users.noreply.github.com>
2023-07-19 13:53:38 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -4,3 +4,39 @@ from PyQt5.QtWidgets import (
        QHBoxLayout, QVBoxLayout,
        QGroupBox, QRadioButton,
        QPushButton, QLabel, QListWidget, QLineEdit)
+
+from instr import *
+from second_win import *
+     
+class MainWin(QWidget):
+   def __init__(self):
+       super().__init__()
+       self.set_appear()
+       self.initUI()
+       self.connects()
+       self.show()
+       
+   def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
+
+   def initUI(self):
+       self.hello_text = QLabel(txt_hello)
+       self.instruction = QLabel(txt_instruction)
+       self.button = QPushButton(txt_next)
+
+       self.layout = QVBoxLayout()
+       self.layout.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+       self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
+       self.layout.addWidget(self.button, alignment = Qt.AlignCenter)         
+       self.setLayout(self.layout)
+  
+   def next_click(self):
+       self.tw = TestWin()
+       self.hide()
+   def connects(self):
+       self.button.clicked.connect(self.next_click)
+app = QApplication([])
+mw = MainWin()
+app.exec_()
