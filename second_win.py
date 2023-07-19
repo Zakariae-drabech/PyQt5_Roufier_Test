fa3e92489655cc31955ffeb900c98f51da09705a
zakariae drabech <52219383+Zakariae-drabech@users.noreply.github.com>
2023-07-19 16:58:03 +0100

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -7,3 +7,18 @@ from PyQt5.QtWidgets import (
        QPushButton, QLabel, QListWidget, QLineEdit)
 
 
+from instr import *
+from final_win import *
+
+class TestWin(QWidget):
+   def __init__(self):
+       ''' the window in which the survey is being conducted '''
+       super().__init__()
+       # sets what the window will look like (label, size, location)
+       self.set_appear()
+       # creating and configuring graphic elements:
+       self.initUI()
+       #establishes connections between elements
+       self.connects()
+       # start:
+       self.show()
