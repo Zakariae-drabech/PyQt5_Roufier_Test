fc1fbd94df0a1e3e13863ac6a642ab371db2fc37
Zakariae Drabech <52219383+Zakariae-drabech@users.noreply.github.com>
2023-07-20 16:38:44 +0100

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -5,17 +5,42 @@ from PyQt5.QtWidgets import (
        QHBoxLayout, QVBoxLayout, QGridLayout,
        QGroupBox, QRadioButton,
        QPushButton, QLabel, QListWidget, QLineEdit)
+      
 from instr import *
 
-
 class FinalWin(QWidget):
-   def __init__(self):
+   def __init__(self, exp):
        ''' the window in which the survey is being conducted '''
        super().__init__()
+       #getting data about the experiment
+       self.exp = exp
        # creating and configuring graphic elements:
        self.initUI()
+
        # sets what the window will look like (label, size, location)
        self.set_appear()
+      
        # start:
        self.show()
+
+   def initUI(self):
+       ''' creates graphic elements '''
+       self.work_text = QLabel(txt_workheart)
+       self.index_text = QLabel(txt_index)
+
+       self.layout_line = QVBoxLayout()
+       self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+       self.layout_line.addWidget(self.work_text, alignment = Qt.AlignCenter)        
+       self.setLayout(self.layout_line)
+
+   ''' sets what the window will look like (label, size, location) '''
+   def set_appear(self):
+       self.setWindowTitle(txt_finalwin)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
+
+
+   def results(self):
+          pass
+
 
