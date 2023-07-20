421f9e8da1ab03a3724ec39bb2dcb49c89620b1a
Zakariae Drabech <52219383+Zakariae-drabech@users.noreply.github.com>
2023-07-20 16:52:34 +0100

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -8,39 +8,48 @@ from PyQt5.QtWidgets import (
       
 from instr import *
 
+
 class FinalWin(QWidget):
-   def __init__(self, exp):
-       ''' the window in which the survey is being conducted '''
-       super().__init__()
-       #getting data about the experiment
-       self.exp = exp
-       # creating and configuring graphic elements:
-       self.initUI()
+    def __init__(self, exp):
+        ''' the window in which the survey is being conducted '''
+        super().__init__()
 
-       # sets what the window will look like (label, size, location)
-       self.set_appear()
-      
-       # start:
-       self.show()
+        #getting data about the experiment
+        self.exp = exp
 
-   def initUI(self):
-       ''' creates graphic elements '''
-       self.work_text = QLabel(txt_workheart)
-       self.index_text = QLabel(txt_index)
+        # creating and configuring graphic elements:
+        self.initUI()
 
-       self.layout_line = QVBoxLayout()
-       self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
-       self.layout_line.addWidget(self.work_text, alignment = Qt.AlignCenter)        
-       self.setLayout(self.layout_line)
-
-   ''' sets what the window will look like (label, size, location) '''
-   def set_appear(self):
-       self.setWindowTitle(txt_finalwin)
-       self.resize(win_width, win_height)
-       self.move(win_x, win_y)
+        # sets what the window will look like (label, size, location)
+        self.set_appear()
+        
+        # start:
+        self.show()
 
 
-   def results(self):
-          pass
+
+    def initUI(self):
+        ''' creates graphic elements '''
+        self.work_text = QLabel(txt_workheart + self.results())
+        self.index_text = QLabel(txt_index + str(self.index))
+        
+        self.work_text.setStyleSheet("font-size: 18px; color: #444;")
+        self.index_text.setStyleSheet("font-size: 18px; color: #444;")
+
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+        self.layout_line.addWidget(self.work_text, alignment = Qt.AlignCenter)        
+        self.setLayout(self.layout_line)
+
+
+    ''' sets what the window will look like (label, size, location) '''
+    def set_appear(self):
+        self.setWindowTitle(txt_finalwin)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def results(self):
+        return "TO DO"
 
 
